285eb96c20248b046ef21039ab6835471cc37faf
Вероника <ollhaillelush@gmail.com>
2020-11-12 14:35:07 +0300

    realize props and render screens

---
+++ src/components/screen-list-item.vue
@@ -0,0 +1,30 @@
+<template>
+  <li class="screen-list__item">
+    <div class="screen-list__light" :style='this.color'></div>
+  </li>
+</template>
+
+<script>
+export default {
+  name: 'screenListItem',
+  props: ['color', 'paleColor', 'timer', 'id']
+}
+</script>
+
+<style>
+  .screen-list__item {
+    background-color: #f4f9fc;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 200px;
+    height: 200px;
+    margin-bottom: 20px;
+  }
+
+  .screen-list__light {
+    border-radius: 50%;
+    width: 50px;
+    height: 50px
+  }
+</style>
---
+++ src/components/screen-list.vue
@@ -0,0 +1,56 @@
+<template>
+  <ul class="screen-list">
+    <screenListItem 
+    v-for='(screen, index) in screens'
+    :key='index'
+    v-bind='screen'/>
+  </ul>
+</template>
+
+
+<script>
+import screenListItem from './screen-list-item'
+
+export default {
+  name: 'screenList',
+  data() {
+    return {
+      screens: [
+        {
+          color: '#E32636',
+          paleColor: '#ffbfb7',
+          timer: 10,
+          id: 'red'
+        },
+        {
+          color: '#ffc61b',
+          paleColor: '#ffebbb',
+          timer: 3,
+          id: 'yellow'
+        },
+        {
+          color: '#8db600',
+          paleColor: '#dde6b5',
+          timer: 15,
+          id: 'green'
+        }                  
+      ]
+    }
+  },
+  components: {
+    screenListItem
+  }
+}
+</script>
+
+<style>
+  .screen-list {
+    margin: 0 auto;
+    padding: 0;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    list-style-type: none;
+  }
+</style>
+
