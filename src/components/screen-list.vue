0452dc8b9b0be2853c4d6258040b18cad832c232
Вероника <ollhaillelush@gmail.com>
2020-11-12 20:35:22 +0300

    Черновик таймера

--- src/components/screen-list.vue
+++ src/components/screen-list.vue
@@ -1,13 +1,19 @@
 <template>
   <ul class="screen-list">
     <li class="screen-list__item">
-      <div class="screen-list_light screen-list_light--red" :class="{active: current=='red'}"></div>
+      <div class="screen-list_light screen-list_light--red" :class="{active: current=='red'}">
+        <span class="screen-list__timer" v-if="current=='red'">{{timeoutCounter}}</span>
+      </div>
     </li>
     <li class="screen-list__item">
-      <div class="screen-list_light screen-list_light--yellow" :class="{active: current=='yellow'}"></div>
+      <div class="screen-list_light screen-list_light--yellow" :class="{active: current=='yellow'}">
+        <span class="screen-list__timer" v-if="current=='yellow'">{{timeoutCounter}}</span>
+      </div>
     </li>
     <li class="screen-list__item">
-      <div class="screen-list_light screen-list_light--green" :class="{active: current=='green'}"></div>
+      <div class="screen-list_light screen-list_light--green" :class="{active: current=='green'}">
+        <span class="screen-list__timer" v-if="current=='green'">{{timeoutCounter}}</span>
+      </div>
     </li>        
     <!-- <li 
     class="screen-list__item" 
@@ -39,7 +45,7 @@ class SwitchController {
     callback(screen)
     setTimeout(()=>{
       this.trigger(screen.next, callback)
-    }, screen.timer * 1000)
+    }, screen.timer * 1005)
   }
 }
 
@@ -47,24 +53,38 @@ export default {
   name: 'screenList',
   data() {
     return {
-      current: 'red'
+      current: 'red',
+      timeoutCounter: Number
+    }
+  },
+  methods: {
+    countDownTimer() {
+      console.log(this.timeoutCounter);
+      if(this.timeoutCounter > 0) {
+          setTimeout(() => {
+            this.timeoutCounter -= 1
+            this.countDownTimer()
+          }, 1000)
+      }
     }
   },
   mounted() {
     const controller = new SwitchController()
     
-    let red = new Screen('red', 10)
+    let red = new Screen('red', 5)
     let yellowToGreen = new Screen('yellow', 3)
     let yellowToRed = new Screen('yellow', 3)    
-    let green = new Screen('green', 15)
+    let green = new Screen('green', 5)
 
     red.next = yellowToGreen
     yellowToGreen.next = green
     green.next = yellowToRed
     yellowToRed.next = red
 
-    controller.trigger(red, (screen) => {
-      this.current = screen.id
+    controller.trigger(red, 
+    (screen) => {
+      this.timeoutCounter = screen.timer
+      this.current = screen.id      
     });
   }
 }
@@ -117,7 +137,7 @@ export default {
   .screen-list__timer 
     font-size: 40px
     margin: auto
-    color: #fff
+    color: #000
   
 </style>
 
